cce742a76510e05d4560564d85006feffa91ccbe
xossiane <anejosi.lima@gmail.com>
2022-08-29 18:50:18 -0300

    Funçoes aplicadas mas o toggle tá ao contrário

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 import './style.css'
 
+const panels = document.querySelectorAll(".panel");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <p> Hello human </p>
-  </div>
-`
+function toggleOpen(){
+  
+  this.classList.toggle('open');
+}
+
+function toggleActive(e){
+  console.log(e.propertyName)
+  if(e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active')
+  }
+ /*   */
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen))
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
 
 
